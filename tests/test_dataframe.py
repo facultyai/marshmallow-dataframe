ded31207444b920ee7658f7c30b914c6bb167185
Víctor Zabalza <vzabalza@gmail.com>
2019-05-14 09:24:12 +0100

    Add failing category columns to test df

--- tests/test_dataframe.py
+++ tests/test_dataframe.py
@@ -23,8 +23,13 @@ def sample_df():
             "string": ["a", "b"],
             "float": [1.1, 2.2],
             "datetime": ["2018-01-01T12:00:00Z", "2018-01-02T12:00:00Z"],
+            "categorical_int": [0, 1],
+            "categorical_str": ["zero", "one"],
         }
     )
+
+    df["categorical_int"] = df["categorical_int"].astype("category")
+    df["categorical_str"] = df["categorical_str"].astype("category")
 
     df["datetime"] = pd.to_datetime(df["datetime"]).astype("datetime64[ns]")
 
